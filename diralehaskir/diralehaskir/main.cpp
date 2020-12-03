1e30a8315710c89dd449a274f2bca928c8e4b14b
sharon <sharonvazana@gmail.com>
2020-12-03 16:28:14 +0200

    func-print ad to file

--- diralehaskir/diralehaskir/main.cpp
+++ diralehaskir/diralehaskir/main.cpp
@@ -12,6 +12,7 @@
 #define MAX_NAME 15 //enough???
 #define MAX_PASSWORD 10 //less???
 #define BUFFER 20
+#define AMENITIES 9
 typedef struct amenities { bool disabledAccess, wifi, kitchen, tv, balcony, washingMachine, 
 airConditioning, swimmingPool, parkingLot;};
 
@@ -19,11 +20,14 @@ typedef struct ad { bool available; string description; int price; int discount;
 int numOfPeople; int numOfRooms; int numOfBeds; amenities ameNities; string attraction; int rating;};
 //also- landlord name, phone number,		and it's own calender??
 
-typedef struct landlord { string fullName; int id; string email; string password; int phoneNumber; int sumOfDeals; int adSize; ad* properties; };
+typedef struct landlord { string fullName; string phoneNumber; string password; int id; 
+string email;  int sumOfDeals; int adSize; ad* properties; };
+
+typedef struct traveler { string fullName; string phoneNumber; string password; };
 using namespace std;
 
 //-------------print to file
-void printToFile(int arrSize, landlord* landlordArr) {
+void printToFile(int landlordSize, landlord* landlordArr, int travelerSize, traveler* travelerArr) {
 	ofstream outFile;
 	try {
 		outFile.open("landlord.data");//open file in ios::out default mode
@@ -39,15 +43,88 @@ void printToFile(int arrSize, landlord* landlordArr) {
 		throw;
 	}
 	//if opening file succeeded
-	outFile << arrSize << endl;//prints size of landlord array
-	for (int i = 0; i < arrSize; ++i) {
+	outFile << landlordSize << endl;//prints size of landlord array
+	int i = 0;
+	for (; i < landlordSize; ++i) {
 		outFile << landlordArr[i].fullName << endl
+			<< landlordArr[i].phoneNumber << endl
+			<< landlordArr[i].password << endl
 			<< landlordArr[i].id << endl
 			<< landlordArr[i].email << endl
-			<< landlordArr[i].password << endl
-			<< landlordArr[i].phoneNumber << endl
 			<< landlordArr[i].sumOfDeals << endl
 			<< landlordArr[i].adSize << endl;
+		printAdsToFile(landlordArr[i].adSize, landlordArr[i].properties);
+		printTravelerToFile(travelerSize, travelerArr);
+	}
+	outFile.close();//close file
+}
+//-------------printAdsToFile
+void printAdsToFile(int size, ad* adsArr) {
+	ofstream outFile;
+	try {
+		outFile.open("properties.data");//open file in ios::out default mode
+		if (!outFile)
+			throw("couldn't open file");
+	}
+	catch (const char* const x) {
+		cout << x << endl;
+		throw;
+	}
+	catch (...) {
+		cout << "ERROR!" << endl;
+		throw;
+	}
+	//if opening file succeeded
+	outFile << size << endl;//prints size of landlord array
+	int i = 0;
+	for (; i < size; ++i) {
+		outFile << adsArr[i].available << endl
+			<< adsArr[i].description << endl
+			<< adsArr[i].price << endl
+			<< adsArr[i].discount << endl
+			<< adsArr[i].location << endl
+			<< adsArr[i].numOfPeople << endl
+			<< adsArr[i].numOfRooms << endl
+			<< adsArr[i].numOfBeds << endl;
+		//print amenities
+		outFile << adsArr[i].ameNities.disabledAccess << endl
+			<< adsArr[i].ameNities.wifi << endl
+			<< adsArr[i].ameNities.kitchen << endl
+			<< adsArr[i].ameNities.tv << endl
+			<< adsArr[i].ameNities.balcony << endl
+			<< adsArr[i].ameNities.washingMachine << endl
+			<< adsArr[i].ameNities.airConditioning << endl
+			<< adsArr[i].ameNities.swimmingPool << endl
+			<< adsArr[i].ameNities.parkingLot << endl;
+		//print rest
+		outFile << adsArr[i].attraction << endl
+			<< adsArr[i].rating << endl;
+	}
+	outFile.close();//close file
+}
+//-------------printTravelerToFile
+void printTravelerToFile(int size, traveler* travelerArr) {
+	ofstream outFile;
+	try {
+		outFile.open("traveler.data");//open file in ios::out default mode
+		if (!outFile)
+			throw("couldn't open file");
+	}
+	catch (const char* const x) {
+		cout << x << endl;
+		throw;
+	}
+	catch (...) {
+		cout << "ERROR!" << endl;
+		throw;
+	}
+	//if opening file succeeded
+	outFile << size << endl;//prints size of landlord array
+	int i = 0;
+	for (; i < size; ++i) {
+		outFile << travelerArr[i].fullName << endl
+			<< travelerArr[i].phoneNumber << endl
+			<< travelerArr[i].password << endl;
 	}
 	outFile.close();//close file
 }
